492d07720cfb341d1de0e98b33419f1ecd56434d
muhammad18939-design <muhammad.18939@zu.edu.pk>
2025-12-17 12:47:00 +0500

    Add configuration settings for SQL and file handling

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# ---------- SQL SERVER CONFIG ----------
+SQL_SERVER = "HUSAF-PF0PKM4Q"
+SQL_DATABASE = "py"      
+SQL_SCHEMA = "dbo"
+
+# ---------- FILE CONFIG ----------
+ALLOWED_EXTENSIONS = ["csv", "xlsx", "xls"]
+TEMP_DIR = "temp_files"
+
+# ---------- STREAMLIT ----------
+APP_TITLE = "SQL Data Ingestion Pipeline"
